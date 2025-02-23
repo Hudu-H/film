5c95bceb83b20f89cf9a1cc298b620f32744fdbb
Hudu <ahmedhudu21@gmail.com>
2025-02-23 19:51:05 +0100

    add mui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 const App = () => {
-	return <div>happy coding!</div>;
+	return (
+		<div>
+			<CssBaseline />
+			<h2>Sup film app</h2>
+		</div>
+	);
 };
 
 export default App;
