77df931e9a2bbf76acf224aa50e11d5be39c176e
Hudu <ahmedhudu21@gmail.com>
2025-04-03 23:51:17 +0200

    implement logout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Box, Typography, Button } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 // internal imports
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
 	const { user } = useSelector(userSelector);
-	console.log(user);
-	return <div>Profile - {user.username}</div>;
+
+	const favouriteMovies = [];
+
+	const logout = () => {
+		localStorage.clear();
+
+		window.location.href = '/';
+	};
+	return (
+		<Box>
+			<Box display="flex" justifyContent="space-between">
+				<Typography variant="h4" gutterBottom>
+					MY Profile
+				</Typography>
+				<Button color="inherit" onClick={logout}>
+					Logout &nbsp; <ExitToApp />
+				</Button>
+			</Box>
+			{!favouriteMovies.length ? (
+				<Typography variant="h5">Add favourites or watchlist some movies.</Typography>
+			) : (
+				<Box>Favourite Movies</Box>
+			)}
+		</Box>
+	);
 };
 
 export default Profile;
