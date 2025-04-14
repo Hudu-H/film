41d110fb52afb6d66cb72583826f52e00f9c4cff
Hudu <ahmedhudu21@gmail.com>
2025-04-14 21:35:23 +0200

    add movie tagline & genre info

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -23,15 +23,19 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
+// import { useSelector, useDispatch } from 'react-redux';
 
 // internal imports
 import { useGetMovieQuery } from '../../services/TMDB';
 import useStyles from './styles';
+import genreIcons from '../../assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const MovieInformation = () => {
 	const { id } = useParams();
 	const { data, isFetching, error } = useGetMovieQuery(id);
 	const classes = useStyles();
+	const dispatch = useDispatch();
 
 	// edge cases
 	if (isFetching) {
@@ -63,6 +67,40 @@ const MovieInformation = () => {
 				<Typography variant="h3" align="center" gutterBottom>
 					{data?.title} ({data.release_date.split('-')[0]})
 				</Typography>
+				<Typography variant="h5" align="center" gutterBottom>
+					{data?.tagline}
+				</Typography>
+				<Grid2 item className={classes.containerSpaceAround}>
+					<Box display="flex" align="center">
+						<Rating readOnly value={data.vote_average / 2} />
+						<Typography gutterBottom variant="subtitle1" style={{ marginLeft: '10px' }}>
+							{data?.vote_average} / 10
+						</Typography>
+					</Box>
+					<Typography gutterBottom variant="h6" align="center">
+						{data?.runtime}min{' '}
+						{data.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+					</Typography>
+				</Grid2>
+				<Grid2 item className={classes.genresContainer}>
+					{data?.genres?.map((genre) => (
+						<Link
+							to="/"
+							key={genre.name}
+							className={classes.links}
+							onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+						>
+							<img
+								src={genreIcons[genre.name.toLowerCase()]}
+								height={30}
+								className={classes.genreImage}
+							/>
+							<Typography variant="subtitle1" color="textPrimary">
+								{genre.name}
+							</Typography>
+						</Link>
+					))}
+				</Grid2>
 			</Grid2>
 		</Grid2>
 	);
--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -1,4 +1,4 @@
-import { Height } from '@mui/icons-material';
+import { Padding } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
 
 // root or general comp styles
@@ -14,18 +14,36 @@ export default makeStyles((theme) => ({
 	},
 	poster: {
 		boxShadow: '0.5em 1em 1em rgb(64, 64, 80)',
-		borderRadius: '20PX',
+		borderRadius: '20px',
 		width: '80%',
+		[theme.breakpoints.down('md')]: {
+			width: '50%',
+			height: '350px',
+			margin: '0 auto',
+		},
 		[theme.breakpoints.down('sm')]: {
 			width: '100%',
 			height: '350px',
 			margin: '0 auto',
 			marginBottom: '30px',
 		},
+	},
+	genresContainer: {
+		margin: '10px 0 !important',
+		display: 'flex',
+		justifyContent: 'space-around',
+		flexWrap: 'wrap',
+	},
+	genreImage: {
+		filter: theme.palette.mode === 'dark' && 'invert(1)',
+		marginRight: '10px',
+	},
+	links: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
 		[theme.breakpoints.down('md')]: {
-			width: '70%',
-			height: '350px',
-			margin: '0 auto',
+			Padding: '0.5rem 1rem',
 		},
 	},
 }));
